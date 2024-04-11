1258ff6671164d3fc748b2f1ff1fd15cc5bf0573
Dangthicamnhi <dangthicamnhi12@gmail.com>
2024-04-11 15:50:28 +0700

    laravel demo code

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,38 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\CrudUserController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
 */
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+Route::get('dashboard', [CrudUserController::class, 'dashboard']);
+
+Route::get('login', [CrudUserController::class, 'login'])->name('login');
+Route::post('login', [CrudUserController::class, 'authUser'])->name('auth.authUser');
+
+Route::get('create', [CrudUserController::class, 'createUser'])->name('auth.createUser');
+Route::post('create', [CrudUserController::class, 'postUser'])->name('auth.postUser');
+
+Route::get('read', [CrudUserController::class, 'readUser'])->name('auth.readUser');
+
+Route::get('delete', [CrudUserController::class, 'deleteUser'])->name('auth.deleteUser');
+
+Route::get('update', [CrudUserController::class, 'updateUser'])->name('auth.updateUser');
+Route::post('update', [CrudUserController::class, 'postUpdateUser'])->name('auth.postUpdateUser');
+
+Route::get('list', [CrudUserController::class, 'listUser'])->name('auth.list');
+
+Route::get('signout', [CrudUserController::class, 'signOut'])->name('signout');
+
+Route::get('/', function () {
+    return view('welcome');
+});
